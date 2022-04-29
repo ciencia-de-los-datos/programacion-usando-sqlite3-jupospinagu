75a3f3fd860195b5c217910f81c166134c951c07
Juan Pablo Ospina Gutierrez <101611634+jupospinagu@users.noreply.github.com>
2022-04-29 13:24:27 -0500

    Update pregunta_11.sql

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -39,9 +39,10 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
 SELECT
-    strftime('%Y', c14), COUNT(strftime('%Y', c14))
+    strftime('%Y', c14) as "YEAR", COUNT('YEAR')
 FROM
     tbl1
 WHERE
-    strftime('%Y', c14) = '2018'
+   YEAR = '2018'
+;
 
